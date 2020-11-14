5ac28b0527d3d641344bb90dfbdebae11e797a52
kryptondv <kryptondevpl@gmail.com>
2020-11-14 12:04:52 +0100

    add global styles

---
+++ src/layouts/globalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: #1C042E;
+    font-family: "Montserrat", sans-serif;
+    color: #fff;
+  }
+`
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,9 +2,12 @@ import React from "react"
 
 import Header from "../components/Header"
 
+import { GlobalStyle } from "./globalStyle"
+
 export default function Layout({ children }) {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <main>{children}</main>
       <footer />
