cc56a02a920c08681e487279255fe25985fb4621
kryptondv <kryptondevpl@gmail.com>
2020-11-14 14:01:28 +0100

    add query for product data in ProductList

--- src/components/HomePage/ProductList/index.js
+++ src/components/HomePage/ProductList/index.js
@@ -1,8 +1,29 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default function ProductList({ heading }) {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        nodes {
+          frontmatter {
+            image {
+              publicURL
+            }
+            slug
+            name
+          }
+          excerpt
+        }
+      }
+    }
+  `)
+
+  const products = data.allMarkdownRemark.nodes.filter(
+    ({ frontmatter }) => !frontmatter.lead
+  )
+
   const { title, lead } = heading
-  console.log({ title, lead })
   return (
     <div>
       <h2>{title}</h2>
