4c6fdf391733c39f405ef06fb3551f8680e6c82f
kryptondv <kryptondevpl@gmail.com>
2020-11-18 12:48:06 +0100

    refactor Button styles

--- src/components/Common/Button/styles.js
+++ src/components/Common/Button/styles.js
@@ -1,35 +1,43 @@
 import styled, { css } from "styled-components"
 
-const big = css`
-  width: 146px;
-  height: 40px;
-  line-height: 16px;
-  font-size: 13px;
-`
-
-const small = css`
-  width: 89px;
-  height: 27px;
-  line-height: 14px;
-  font-size: 12px;
-`
-const round = css`
-  width: 44px;
-  height: 44px;
-  border-radius: 50%;
-  line-height: 37px;
-  font-size: 32px;
-`
+const type = {
+  big: css`
+    width: 146px;
+    height: 40px;
+    line-height: 16px;
+    font-size: 13px;
+  `,
+  small: css`
+    width: 89px;
+    height: 27px;
+    line-height: 14px;
+    font-size: 12px;
+  `,
+  round: css`
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    line-height: 37px;
+    font-size: 32px;
+  `,
+}
 
 export const StyledButton = styled.button`
-  ${(props) => props.big && big};
-  ${(props) => props.small && small};
-  ${(props) => props.round && round};
+/* add styles for button type */
+  ${(props) => {
+    for (const key in type) {
+      if (props[key]) {
+        return type[key]
+      }
+    }
+  }}
+  
   border: none;
   border-radius: ${(props) => props.borderRadius && "4px"};
   background: #ab528d;
   color: ${(props) => (props.secondaryColor ? "#eeeeee" : "#fff")};
-  font-family: ${(props) => props.secondaryFamily ? "Roboto, sans-serif" : "Montserrat, sans-serif"};
+  font-family: ${(props) =>
+    props.secondaryFamily ? "Roboto, sans-serif" : "Montserrat, sans-serif"};
   font-weight: ${(props) => (props.bold ? "700" : "400")};
   text-transform: uppercase;
   transition: 0.2s ease-out;
